a780164e3d601ec6238abdbcbdceece23cadd34b
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2022-10-18 15:06:48 -0500

    tracking DBG dwca; related to https://github.com/bio-guoda/preston/issues/193; fyi @mielliott

---
+++ track-dwca.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#
+
+preston track https://intermountainbiota.org/portal/content/dwca/DBG-KHD_DwC-A.zip\
+ http://ipt.vertnet.org:8080/ipt/archive.do?r=dbg_chatfieldfarms_riparianrestoration\
+ http://scan-bugs.org/portal/content/dwca/DBG-DBGA_DwC-A.zip\
+ http://ipt.vertnet.org:8080/ipt/archive.do?r=dbg_mirabilis-rotundifolia_demogarphicmonitoring\
+ http://ipt.vertnet.org:8080/ipt/archive.do?r=dbg_physaria-bellii_demographicmonitoring
+
+
+# | preston dwc-stream
+
